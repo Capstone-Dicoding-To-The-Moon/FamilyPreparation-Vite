8b328c398edb55a1772fa783c2aff289eb76aa54
desihamita <69859667+desihamita@users.noreply.github.com>
2022-11-23 20:10:48 +0700

    Create Page Profile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Artikel from './pages/Artikel';
 import About from './pages/About';
 import ForumDiskusi from './pages/ForumDiskusi';
+import Profile from './pages/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/NavbarComponent';
 import FooterComponent from './components/FooterComponent';
@@ -16,6 +17,7 @@ function App() {
         <Route path="/artikel" element={<Artikel />} />
         <Route path='/about' element={<About />}/>
         <Route path="/forumDiskusi" element={<ForumDiskusi />} />
+        <Route path='/profile' element={<Profile />}/>
         <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
       <FooterComponent />
